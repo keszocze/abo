5f2939620361fa3a06bd0ee9e8b078354e434451
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-02-16 21:16:34 +0100

    Implement aig parser

--- src/parsing/aig_parser.cpp
+++ src/parsing/aig_parser.cpp
@@ -3,3 +3,107 @@
 //
 
 #include "aig_parser.hpp"
+
+#include <cassert>
+
+namespace abo::parser {
+
+    void aig_parser::on_header( std::size_t m, std::size_t i, std::size_t, std::size_t o, std::size_t a ) const {
+        num_variables = m;
+        num_literals = m * 2 + 2;
+        literals.resize(num_literals);
+        literals[0] = mgr.bddZero();
+        literals[1] = mgr.bddOne();
+
+        num_inputs = i;
+        input_names.resize(num_inputs);
+        num_outputs = o;
+        output_indices.resize(num_outputs);
+        output_names.resize(num_outputs);
+        num_gates = a;
+    }
+
+    void aig_parser::on_input( unsigned index, unsigned lit ) const {
+        assert(index < num_inputs);
+        assert(lit < num_literals);
+
+        literals[lit] = mgr.bddVar(static_cast<int>(index));
+    }
+
+    void aig_parser::on_output( unsigned index, unsigned lit ) const {
+        assert(index < num_outputs);
+        assert(lit < num_literals);
+
+        output_indices[index] = lit;
+    }
+
+    void aig_parser::on_and( unsigned index, unsigned left_lit, unsigned right_lit ) const {
+        assert(index * 2 <= num_literals);
+        assert(left_lit < num_literals && right_lit < num_literals);
+        // duplicate assignment of the same variable makes no sense
+        assert(!literals[index * 2]);
+
+        const BDD &left = find_literal(left_lit);
+        const BDD &right = find_literal(right_lit);
+        literals[index * 2] = left & right;
+    }
+
+    void aig_parser::on_input_name( unsigned index, const std::string& name ) const {
+        assert(index <= num_inputs);
+
+        input_names[index] = name;
+    }
+
+    void aig_parser::on_output_name( unsigned index, const std::string& name ) const {
+        assert(index <= num_outputs);
+
+        output_names[index] = name;
+    }
+
+    const std::vector<BDD> aig_parser::get_outputs() const {
+        std::vector<BDD> result;
+
+        for (unsigned output : output_indices) {
+            result.push_back(find_literal(output));
+        }
+
+        return result;
+    }
+
+    const BDD & aig_parser::find_literal( unsigned literal ) const {
+        if (literals[literal]) {
+            return literals[literal].value();
+        }
+
+        // inverted literal
+        if (literal & 1 && literals[literal - 1]) {
+            const BDD &original = literals[literal - 1].value();
+            literals[literal] = !original;
+            return literals[literal].value();
+        }
+
+        // literal is not known yet, they should be created in order
+        assert(false);
+    }
+
+    // unsupported operations
+    void aig_parser::on_latch( unsigned, unsigned, latch_init_value ) const {
+        assert(false);
+    }
+
+    void aig_parser::on_bad_state( unsigned, unsigned ) const {
+        assert(false);
+    }
+
+    void aig_parser::on_constraint( unsigned, unsigned ) const {
+        assert(false);
+    }
+
+    void aig_parser::on_fairness( unsigned, unsigned ) const {
+        assert(false);
+    }
+
+    void aig_parser::on_justice( unsigned, const std::vector<unsigned>& ) const {
+        assert(false);
+    }
+}
--- src/parsing/aig_parser.hpp
+++ src/parsing/aig_parser.hpp
@@ -5,10 +5,71 @@
 #ifndef ABO_AIG_PARSER_HPP
 #define ABO_AIG_PARSER_HPP
 
+#include <lorina/aiger.hpp>
+#include <cudd/cplusplus/cuddObj.hh>
+#include <vector>
+#include <optional>
 
-class aig_parser {
+namespace abo::parser {
 
-};
+    class aig_parser : public lorina::aiger_reader {
+    private:
+        const Cudd &mgr;
 
+        // TODO: muss das noch entfernt werden?
+        mutable std::size_t num_variables = 0;
+        mutable std::size_t num_literals = 0;
+        mutable std::size_t num_inputs = 0;
+        mutable std::size_t num_outputs = 0;
+        mutable std::size_t num_gates = 0;
+
+        mutable std::vector<unsigned> output_indices;
+        mutable std::vector<std::string> input_names;
+        mutable std::vector<std::string> output_names;
+        mutable std::vector<std::optional<BDD>> literals;
+
+    public:
+
+        const std::vector<BDD> get_outputs() const;
+
+        const std::vector<std::string> & get_input_names() const { return input_names; }
+
+        const std::vector<std::string> & get_output_names() const { return output_names; }
+
+        aig_parser( Cudd &mgr ) :
+                        mgr(mgr) {}
+
+        virtual ~aig_parser() = default;
+
+        virtual void on_header( std::size_t m, std::size_t i, std::size_t l, std::size_t o, std::size_t a ) const override;
+
+        virtual void on_input( unsigned index, unsigned lit ) const override;
+
+        virtual void on_output( unsigned index, unsigned lit ) const override;
+
+        virtual void on_and( unsigned index, unsigned left_lit, unsigned right_lit ) const override;
+
+        virtual void on_input_name( unsigned index, const std::string& name ) const override;
+
+        virtual void on_output_name( unsigned index, const std::string& name ) const override;
+
+        // unsupported operations
+        virtual void on_latch( unsigned index, unsigned next, latch_init_value reset ) const override;
+
+        virtual void on_bad_state( unsigned index, unsigned lit ) const override;
+
+        virtual void on_constraint( unsigned index, unsigned lit ) const override;
+
+        virtual void on_fairness( unsigned index, unsigned lit ) const override;
+
+        virtual void on_justice( unsigned index, const std::vector<unsigned>& lits ) const override;
+
+    private:
+
+        const BDD & find_literal( unsigned literal ) const;
+
+    };
+
+}
 
 #endif //ABO_AIG_PARSER_HPP
