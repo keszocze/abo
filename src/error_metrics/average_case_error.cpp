89fef92e4d8c2f61fe2e162168775e82c39d5efe
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-04-09 08:58:03 +0200

    Implement mean squared error
    As it is closely related to the average case error, it is in the same
    file.

--- src/error_metrics/average_case_error.cpp
+++ src/error_metrics/average_case_error.cpp
@@ -25,6 +25,31 @@ namespace abo::error_metrics {
                 boost::multiprecision::cpp_dec_float_100(one << max_support_size);
     }
 
+    boost::multiprecision::cpp_dec_float_100 mean_squared_value(const std::vector<BDD> &f) {
+
+        boost::multiprecision::uint256_t sum = 0;
+        const boost::multiprecision::uint256_t one = 1;
+
+        int max_support_size = 0;
+        for (const auto &bdd : f) {
+            max_support_size = std::max(max_support_size, bdd.SupportSize());
+        }
+
+        for (unsigned int i = 0;i<f.size();i++) {
+            for (unsigned int b = i;b<f.size();b++) {
+                // cudd handles the case i = b efficiently
+                BDD both = f[i] & f[b];
+                double minterms = both.CountMinterm(max_support_size);
+                // most terms (except for i == b), appear twice when factoring the square
+                boost::multiprecision::uint256_t doubling_factor = i == b ? 1 : 2;
+                sum += (one << i) * (one << b) * boost::multiprecision::uint256_t(minterms) * doubling_factor;
+            }
+        }
+
+        return boost::multiprecision::cpp_dec_float_100(sum) /
+                boost::multiprecision::cpp_dec_float_100(one << max_support_size);
+    }
+
     boost::multiprecision::cpp_dec_float_100 average_case_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat) {
         std::vector<BDD> f_= f;
         std::vector<BDD> f_hat_ = f_hat;
@@ -37,6 +62,20 @@ namespace abo::error_metrics {
         std::vector<BDD> absolute_difference = abo::util::abs(mgr,difference);
 
         return average_value(absolute_difference);
+    }
+
+    boost::multiprecision::cpp_dec_float_100 mean_squared_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat) {
+        std::vector<BDD> f_= f;
+        std::vector<BDD> f_hat_ = f_hat;
+
+        // add sign bits so that
+        f_.push_back(mgr.bddZero());
+        f_hat_.push_back(mgr.bddZero());
+
+        std::vector<BDD> difference = abo::util::bdd_subtract(mgr, f_, f_hat_);
+        std::vector<BDD> absolute_difference = abo::util::abs(mgr,difference);
+
+        return mean_squared_value(absolute_difference);
     }
 
 }
--- src/error_metrics/average_case_error.hpp
+++ src/error_metrics/average_case_error.hpp
@@ -9,7 +9,11 @@ namespace abo::error_metrics {
 
     boost::multiprecision::cpp_dec_float_100 average_value(const std::vector<BDD> &f);
 
+    boost::multiprecision::cpp_dec_float_100 mean_squared_value(const std::vector<BDD> &f);
+
     boost::multiprecision::cpp_dec_float_100 average_case_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
+
+    boost::multiprecision::cpp_dec_float_100 mean_squared_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
 }
 
