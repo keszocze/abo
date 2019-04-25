4841570f0277f21e704c3802f6734264cbb8d55e
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-04-25 14:35:40 +0200

    Implement generic accuracy reconfigurable adder generator

--- src/example_bdds/approximate_adders.cpp
+++ src/example_bdds/approximate_adders.cpp
@@ -159,6 +159,63 @@ namespace abo::example_bdds {
         return sum;
     }
 
+    std::vector<BDD> generic_accuracy_reconfigurable_adder(Cudd &mgr, unsigned int num_bits, unsigned int resultant_bits,
+                                                           unsigned int previous_bits) {
+
+        std::vector<BDD> input1, input2;
+        input1.reserve(num_bits);
+        input2.reserve(num_bits);
+        for (unsigned int i = 0;i<num_bits;i++) {
+            input1.push_back(mgr.bddVar(i));
+            input2.push_back(mgr.bddVar(i + num_bits));
+        }
+
+        unsigned int sub_adder_length = resultant_bits + previous_bits;
+
+        std::vector<BDD> carry_outs;
+        carry_outs.reserve(num_bits);
+        for (unsigned int i = 0;i<sub_adder_length-1;i++) {
+            if (i == 0) {
+                carry_outs.push_back(carry_generate(input1[i], input2[i], mgr.bddZero()));
+            } else {
+                carry_outs.push_back(carry_generate(input1[i], input2[i], carry_outs.back()));
+            }
+        }
+
+        unsigned int count = resultant_bits;
+        BDD temp_carry = mgr.bddZero();
+        for (unsigned int i = sub_adder_length-1;i<num_bits;i++) {
+            if (count == resultant_bits && i != num_bits-1) {
+                count = 0;
+                temp_carry = mgr.bddZero();
+                for (int j = previous_bits-1;j >= 0;j--) {
+                    temp_carry = carry_generate(input1[i-j], input2[i-j], temp_carry);
+                }
+                carry_outs.push_back(temp_carry);
+            } else {
+                carry_outs.push_back(carry_generate(input1[i], input2[i], carry_outs.back()));
+            }
+            count++;
+        }
+
+        std::vector<BDD> sum;
+        sum.reserve(num_bits + 1);
+        for (unsigned int i = 0;i<num_bits;i++) {
+            BDD a = input1[i];
+            BDD b = input2[i];
+            if (i == 0) {
+                sum.push_back(a ^ (b ^ mgr.bddZero()));
+            } else {
+                sum.push_back(a ^ (b ^ carry_outs[i-1]));
+            }
+        }
+
+        // final carry out
+        sum.push_back(carry_outs.back());
+
+        return sum;
+    }
+
     std::vector<BDD> regular_adder(const Cudd &mgr, unsigned int num_bits) {
 
         std::vector<BDD> input1, input2;
--- src/example_bdds/approximate_adders.hpp
+++ src/example_bdds/approximate_adders.hpp
@@ -11,12 +11,15 @@ namespace abo::example_bdds {
      */
 
     // num_bits is sometimes referred to as 'N', while partition size can be 'Q'
-    std::vector <BDD> almost_correct_adder_1(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
+    std::vector<BDD> almost_correct_adder_1(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
 
     // partition size must be an even number
-    std::vector <BDD> almost_correct_adder_2(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
+    std::vector<BDD> almost_correct_adder_2(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
 
-    std::vector <BDD> gracefully_degrading_adder(Cudd &mgr, unsigned int num_bits, unsigned int num_partitions, unsigned int prediction_bits);
+    std::vector<BDD> gracefully_degrading_adder(Cudd &mgr, unsigned int num_bits, unsigned int num_partitions, unsigned int prediction_bits);
+
+    std::vector<BDD> generic_accuracy_reconfigurable_adder(Cudd &mgr, unsigned int num_bits, unsigned int resultant_bits,
+                                                           unsigned int previous_bits);
 
     std::vector<BDD> regular_adder(const Cudd &mgr, unsigned int num_bits);
 
