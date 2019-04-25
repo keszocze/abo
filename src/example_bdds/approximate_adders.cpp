43266aca6a02b06253f738406c120fea1c104001
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-04-25 10:15:38 +0200

    Implement almost correct adder 2

--- src/example_bdds/approximate_adders.cpp
+++ src/example_bdds/approximate_adders.cpp
@@ -59,6 +59,59 @@ namespace abo::example_bdds {
         return sum;
     }
 
+    std::vector <BDD> almost_correct_adder_2(Cudd &mgr, unsigned int num_bits, unsigned int partition_size) {
+        assert(num_bits >= partition_size && partition_size > 0);
+
+        std::vector<BDD> input1, input2;
+        input1.reserve(num_bits);
+        input2.reserve(num_bits);
+        for (unsigned int i = 0;i<num_bits;i++) {
+            input1.push_back(mgr.bddVar(i));
+            input2.push_back(mgr.bddVar(i + num_bits));
+        }
+
+        unsigned int num_partitions = (num_bits / partition_size) + 1;
+
+        std::vector<BDD> carry_outs;
+        carry_outs.reserve(num_bits);
+
+        for (unsigned int i = 0;i<num_bits;i++) {
+            const BDD &a = input1[i];
+            const BDD &b = input2[i];
+
+            if (i == 0) {
+                carry_outs.push_back(carry_generate(a, b, mgr.bddZero()));
+            } else if (((i+1) % (num_bits / num_partitions) == 0) && (i+1) != num_bits) {
+                BDD temp_carry = mgr.bddZero();
+                for (int j = (partition_size / 2) - 1;j >= 0;j--) {
+                    if (i - j >= 0) {
+                        temp_carry = carry_generate(input1[i-j], input2[i-j], temp_carry);
+                    }
+                }
+                carry_outs.push_back(temp_carry);
+            } else {
+                carry_outs.push_back(carry_generate(a, b, carry_outs.back()));
+            }
+        }
+
+        std::vector<BDD> sum;
+        sum.reserve(num_bits + 1);
+        for (unsigned int i = 0;i<num_bits;i++) {
+            BDD a = input1[i];
+            BDD b = input2[i];
+            if (i == 0) {
+                sum.push_back(a ^ (b ^ mgr.bddZero()));
+            } else {
+                sum.push_back(a ^ (b ^ carry_outs[i-1]));
+            }
+        }
+
+        // final carry out
+        sum.push_back(carry_outs.back());
+
+        return sum;
+    }
+
     std::vector<BDD> regular_adder(const Cudd &mgr, unsigned int num_bits) {
 
         std::vector<BDD> input1, input2;
--- src/example_bdds/approximate_adders.hpp
+++ src/example_bdds/approximate_adders.hpp
@@ -13,6 +13,9 @@ namespace abo::example_bdds {
     // num_bits is sometimes referred to as 'N', while partition size can be 'Q'
     std::vector <BDD> almost_correct_adder_1(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
 
+    // partition size must be an even number
+    std::vector <BDD> almost_correct_adder_2(Cudd &mgr, unsigned int num_bits, unsigned int partition_size);
+
     std::vector<BDD> regular_adder(const Cudd &mgr, unsigned int num_bits);
 
 }
