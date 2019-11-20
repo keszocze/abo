badcd851254e796a299f5c02c39aa9ceb8cf2af8
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-11-20 15:04:10 +0100

    Add documentation for average case error metrics

--- src/error_metrics/average_case_error.hpp
+++ src/error_metrics/average_case_error.hpp
@@ -7,17 +7,64 @@
 
 namespace abo::error_metrics {
 
+    /**
+     * @brief Computes the average value of the function f over all possible inputs
+     * @param f The function to compute the average of
+     * Is interpreted to be unsigned
+     * @return the average value of f as a high precision float
+     */
     boost::multiprecision::cpp_dec_float_100 average_value(const std::vector<BDD> &f);
 
+    /**
+     * @brief Computes the average squared value of the function f over all possible inputs
+     * @param f The function to compute the average squared value of
+     * Is interpreted to be unsigned
+     * @return the mean squared value of f as a high precision float
+     */
     boost::multiprecision::cpp_dec_float_100 mean_squared_value(const std::vector<BDD> &f);
 
+    /**
+     * @brief Computes the average absolute difference between the functions f and f_hat
+     * The computation is performed symbolically with BDD forests and may take exponential time
+     * @param mgr Cudd manager object
+     * @param f The original function (interpreted as an unsigned integer)
+     * @param f_hat The approximated function (interpreted as an unsigned integer)
+     * @return The average absolute difference between f and f_hat
+     */
     boost::multiprecision::cpp_dec_float_100 average_case_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
+    /**
+     * @brief Computes the average squared absolute difference between the functions f and f_hat
+     * The computation is performed symbolically with BDD forests and may take exponential time
+     * @param mgr Cudd manager object
+     * @param f The original function (interpreted as an unsigned integer)
+     * @param f_hat The approximated function (interpreted as an unsigned integer)
+     * @return The average squared absolute difference between f and f_hat
+     */
     boost::multiprecision::cpp_dec_float_100 mean_squared_error(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
-    // compute the same values using an add as an intermediate step
+    /**
+     * @brief Computes the average absolute difference between the functions f and f_hat
+     * The computation is performed symbolically with ADDs and may take exponential time
+     * This function is generally slower than the equivalent average_case_error() function
+     * and should only be used as a reference
+     * @param mgr Cudd manager object
+     * @param f The original function (interpreted as an unsigned integer)
+     * @param f_hat The approximated function (interpreted as an unsigned integer)
+     * @return The average absolute difference between f and f_hat
+     */
     boost::multiprecision::cpp_dec_float_100 average_case_error_add(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
+    /**
+     * @brief Computes the average squared absolute difference between the functions f and f_hat
+     * The computation is performed symbolically with ADDs and may take exponential time
+     * This function is generally slower than the equivalent mean_squared_error() function
+     * and should only be used as a reference
+     * @param mgr Cudd manager object
+     * @param f The original function (interpreted as an unsigned integer)
+     * @param f_hat The approximated function (interpreted as an unsigned integer)
+     * @return The average squared absolute difference between f and f_hat
+     */
     boost::multiprecision::cpp_dec_float_100 mean_squared_error_add(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
 }
