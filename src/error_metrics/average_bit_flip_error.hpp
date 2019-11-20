e5009f48f1f82beac313f43e70b0dc710836d897
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-11-20 14:52:40 +0100

    Add documentation for average bit flip error

--- src/error_metrics/average_bit_flip_error.hpp
+++ src/error_metrics/average_bit_flip_error.hpp
@@ -6,8 +6,24 @@
 
 namespace abo::error_metrics {
 
+    /**
+     * @brief Compute the average bit flip error between the functions f and f_hat
+     * It is computed symbolically using BDD forests.
+     * The time complexity of this function is linear in the number of BDD nodes in the inputs.
+     * @param f The original function
+     * @param f_hat The approximated function. must have the same number of bits as f
+     * @return the average bit flip error ranging between 0 and f.size()
+     */
     double average_bit_flip_error(const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
+    /**
+     * @brief Compute the average bit flip error between the functions f and f_hat
+     * It is computed symbolically using ADDs.
+     * This function may take exponential time in the number of BDD nodes in the input.
+     * @param f The original function
+     * @param f_hat The approximated function. must have the same number of bits as f
+     * @return the average bit flip error ranging between 0 and f.size()
+     */
     double average_bit_flip_error_add(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &f_hat);
 
 }
