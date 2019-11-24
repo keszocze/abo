keszocze/abo

commit e988a8361726e8d884a91cd48f3d35c0a83542a3

Simplify approximate average and maximum relative value computation

src/error_metrics/average_case_relative_error.cpp
@@ -8,13 +8,15 @@ namespace abo::error_metrics {
             average_relative_value(const Cudd &mgr, const std::vector<BDD> &f, const std::vector<BDD> &g) {
 
         BDD zero_so_far = mgr.bddOne();
+
         boost::multiprecision::cpp_dec_float_100 min_average_result = 0;
         boost::multiprecision::cpp_dec_float_100 max_average_result = 0;
-        for (int i = g.size()-1;i>=0;i--) {
+        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, f);
+        for (int i = int(g.size())-1;i>=0;i--) {
             std::vector<BDD> partial_result;
-            partial_result.reserve(f.size());
+            partial_result.reserve(max_one.size());
             BDD modifier = zero_so_far & g[i];
-            for (const BDD &b : f) {
+            for (const BDD &b : max_one) {
                 partial_result.push_back(b & modifier);
             }
             auto average = average_value(partial_result);
@@ -23,15 +25,6 @@ namespace abo::error_metrics {
 
             zero_so_far &= !g[i];
         }
-
-        // handle f(x) = 0
-        std::vector<BDD> partial_result;
-        for (const BDD &b : f) {
-            partial_result.push_back(b & zero_so_far);
-        }
-        auto average = average_value(partial_result);
-        min_average_result += average;
-        max_average_result += average;
 
         return {min_average_result, max_average_result};
     }

src/error_metrics/worst_case_relative_error.cpp
@@ -82,11 +82,12 @@ namespace abo::error_metrics {
         BDD zero_so_far = mgr.bddOne();
         boost::multiprecision::cpp_dec_float_100 min_error = std::numeric_limits<double>::infinity();
         boost::multiprecision::cpp_dec_float_100 max_error = 0;
-        for (int i = g.size()-1;i>=0;i--) {
+        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, f);
+        for (int i = int(g.size())-1;i>=0;i--) {
             std::vector<BDD> partial_result;
-            partial_result.reserve(f.size());
+            partial_result.reserve(max_one.size());
             BDD modifier = zero_so_far & g[i];
-            for (const BDD &b : f) {
+            for (const BDD &b : max_one) {
                 partial_result.push_back(b & modifier);
             }
             boost::multiprecision::cpp_dec_float_100 max_val = boost::multiprecision::cpp_dec_float_100(get_max_value(mgr, partial_result));
@@ -96,15 +97,6 @@ namespace abo::error_metrics {
 
             zero_so_far &= !g[i];
         }
-
-        // handle f(x) = 0
-        std::vector<BDD> partial_result;
-        for (const BDD &b : f) {
-            partial_result.push_back(b & zero_so_far);
-        }
-        boost::multiprecision::cpp_dec_float_100 max_val = boost::multiprecision::cpp_dec_float_100(get_max_value(mgr, partial_result));
-        min_error = min(min_error, max_val); // divided by one
-        max_error = max(max_error, max_val);
 
         return {min_error, max_error};
     }

commit 3d96461e1575136a3047f99aabd2feab8da42152

average-/ worst case relative error: fix bug introduced in e988a836

src/error_metrics/average_case_relative_error.cpp
@@ -11,19 +11,19 @@ namespace abo::error_metrics {
 
         boost::multiprecision::cpp_dec_float_100 min_average_result = 0;
         boost::multiprecision::cpp_dec_float_100 max_average_result = 0;
-        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, f);
+        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, g);
         for (int i = int(g.size())-1;i>=0;i--) {
             std::vector<BDD> partial_result;
             partial_result.reserve(max_one.size());
-            BDD modifier = zero_so_far & g[i];
-            for (const BDD &b : max_one) {
+            BDD modifier = zero_so_far & max_one[i];
+            for (const BDD &b : f) {
                 partial_result.push_back(b & modifier);
             }
             auto average = average_value(partial_result);
             min_average_result += average  / (std::pow(2.0, i + 1) - 1);
             max_average_result += average  / std::pow(2.0, i);
 
-            zero_so_far &= !g[i];
+            zero_so_far &= !max_one[i];
         }
 
         return {min_average_result, max_average_result};

src/error_metrics/worst_case_relative_error.cpp
@@ -82,13 +82,13 @@ namespace abo::error_metrics {
         BDD zero_so_far = mgr.bddOne();
         boost::multiprecision::cpp_dec_float_100 min_error = std::numeric_limits<double>::infinity();
         boost::multiprecision::cpp_dec_float_100 max_error = 0;
-        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, f);
+        std::vector<BDD> max_one = abo::util::bdd_max_one(mgr, g);
         for (int i = int(g.size())-1;i>=0;i--) {
             std::vector<BDD> partial_result;
-            partial_result.reserve(max_one.size());
-            BDD modifier = zero_so_far & g[i];
+            partial_result.reserve(f.size());
+            BDD modifier = zero_so_far & max_one[i];
 
-            for (const BDD &b : max_one) {
+            for (const BDD &b : f) {
                 partial_result.push_back(b & modifier);
             }
             boost::multiprecision::cpp_dec_float_100 max_val = boost::multiprecision::cpp_dec_float_100(get_max_value(mgr, partial_result));
@@ -96,7 +96,7 @@ namespace abo::error_metrics {
             min_error = max(min_error, max_val / (std::pow(2.0, i + 1) - 1));
             max_error = max(max_error, max_val  / std::pow(2.0, i));
 
-            zero_so_far &= !g[i];
+            zero_so_far &= !max_one[i];
         }
 
         return {min_error, max_error};