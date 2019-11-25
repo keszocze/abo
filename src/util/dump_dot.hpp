f675640ae630667e9024dfd02f0a2e232763eb2e
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-11-25 15:08:29 +0100

    Add doc comments for dump dot functions

--- src/util/dump_dot.hpp
+++ src/util/dump_dot.hpp
@@ -18,9 +18,29 @@ namespace abo::util {
         float rank_seperation = 0.25f;
     };
 
+    /**
+     * @brief Dump the given BDD forest as text that can be given to dot. The graph created by this function
+     * is more readable than the Cudd variant as no inverted edges are used. The corresponding variable is used as a nodes text
+     * @param mgr The BDD object manager
+     * @param bdds The functions to show in the dot file
+     * @param function_names The names of the functions. Will be default populated to f_i for the i'th function
+     * if an empty vector is given. Must always either be exactly the same length as bdds or empty
+     * @param output The stream to write the result to
+     * @param conf The presentation parameters like shapes and distances
+     */
     void dump_dot_readable(const Cudd &mgr, const std::vector<BDD> &bdds, const std::vector<std::string> &function_names,
                            std::ostream &output, DotPresentationConfig conf = {});
 
+    /**
+     * @brief Dump the given BDD forest to a file that can be given to dot. The graph created by this function
+     * is more readable than the Cudd variant as no inverted edges are used. The corresponding variable is used as a nodes text
+     * @param mgr The BDD object manager
+     * @param bdds The functions to show in the dot file
+     * @param function_names The names of the functions. Will be default populated to f_i for the i'th function
+     * if an empty vector is given. Must always either be exactly the same length as bdds or empty
+     * @param filename The name of the file to output the result to. Must be writable
+     * @param conf The presentation parameters like shapes and distances
+     */
     void dump_dot_readable_to_file(const Cudd &mgr, const std::vector<BDD> &bdds, const std::vector<std::string> &function_names,
                                    std::string filename, DotPresentationConfig conf = {});
 
