e72e8e11f96eda819840d1c8ff618bf21352e567
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-11-25 12:33:35 +0100

    Allow dumping dot to arbitrary output stream
    And add helper function to write to file

--- src/util/dump_dot.cpp
+++ src/util/dump_dot.cpp
@@ -34,57 +34,55 @@ namespace abo::util {
         }
     }
 
-    void dump_dot_readable(const Cudd &mgr, const BDD &b, std::string filename) {
-        std::ofstream outfile;
-        outfile.open(filename, std::ios::out | std::ios::trunc );
+    void dump_dot_readable(const Cudd &mgr, const BDD &b, std::ostream &output) {
 
         std::vector<std::vector<DdNode*>> level_nodes;
         std::set<DdNode*> visited;
         collect_nodes_rec(b.getNode(), visited, level_nodes);
 
         int indentation = 0;
-        std::function<void()> indent([&]() { for (int i = 0;i<indentation;i++) outfile <<"\t"; });
-        outfile <<"digraph \"DD\"{"<<std::endl;
+        std::function<void()> indent([&]() { for (int i = 0;i<indentation;i++) output <<"\t"; });
+        output <<"digraph \"DD\"{"<<std::endl;
         indentation++;
 
-        indent(); outfile <<"size = \"5,10\""<<std::endl;
-        indent(); outfile <<"center = true;"<<std::endl;
-        indent(); outfile <<"edge [dir = non];"<<std::endl;
-        indent(); outfile <<"nodesep = \"0.2\";"<<std::endl;
-        indent(); outfile <<"ranksep = \"0.25\";"<<std::endl;
-        indent(); outfile <<"margin=0;"<<std::endl;
+        indent(); output <<"size = \"5,10\""<<std::endl;
+        indent(); output <<"center = true;"<<std::endl;
+        indent(); output <<"edge [dir = non];"<<std::endl;
+        indent(); output <<"nodesep = \"0.2\";"<<std::endl;
+        indent(); output <<"ranksep = \"0.25\";"<<std::endl;
+        indent(); output <<"margin=0;"<<std::endl;
 
-        indent(); outfile <<"{"<<std::endl;
+        indent(); output <<"{"<<std::endl;
         indentation++;
 
-        indent(); outfile <<"edge [style = invis];"<<std::endl;
+        indent(); output <<"edge [style = invis];"<<std::endl;
         indent();
         for (unsigned int i = 0;i<level_nodes.size();i++) {
             if (level_nodes[i].size() > 0) {
-                outfile <<"\""<<level_nodes[i][0]<<"\" -> ";
+                output <<"\""<<level_nodes[i][0]<<"\" -> ";
             }
         }
-        outfile <<"\""<<mgr.bddOne().getNode()<<"\";"<<std::endl;
+        output <<"\""<<mgr.bddOne().getNode()<<"\";"<<std::endl;
 
         indentation--;
-        indent(); outfile <<"}"<<std::endl;
+        indent(); output <<"}"<<std::endl;
 
         for (unsigned int i = 0;i<level_nodes.size();i++) {
             if (level_nodes[i].size() > 0) {
-                indent(); outfile <<"{"<<std::endl;
+                indent(); output <<"{"<<std::endl;
                 indentation++;
-                indent(); outfile <<"rank = same;"<<std::endl;
+                indent(); output <<"rank = same;"<<std::endl;
                 for (DdNode *n : level_nodes[i]) {
-                    indent(); outfile <<"\""<<n<<"\" [label = <<I>x&#832"<<i+1<<";</I>>, shape = circle, fixedsize=true, width=0.35];"<<std::endl;
+                    indent(); output <<"\""<<n<<"\" [label = <<I>x&#832"<<i+1<<";</I>>, shape = circle, fixedsize=true, width=0.35];"<<std::endl;
                 }
                 indentation--;
-                indent(); outfile <<"}"<<std::endl;
+                indent(); output <<"}"<<std::endl;
             }
         }
 
-        indent(); outfile <<"{ ranke = same;"<<std::endl;
-        indent(); outfile <<"node [shape = box]; \""<<mgr.bddOne().getNode()<<"\" [label = <&#8868;>, shape = square, fixedsize=true, width=0.35];"<<std::endl;
-        indent(); outfile <<"\""<<mgr.bddZero().getNode()<<"\" [label = <&#8869;>, shape = square, fixedsize=true, width=0.35]; }"<<std::endl;
+        indent(); output <<"{ ranke = same;"<<std::endl;
+        indent(); output <<"node [shape = box]; \""<<mgr.bddOne().getNode()<<"\" [label = <&#8868;>, shape = square, fixedsize=true, width=0.35];"<<std::endl;
+        indent(); output <<"\""<<mgr.bddZero().getNode()<<"\" [label = <&#8869;>, shape = square, fixedsize=true, width=0.35]; }"<<std::endl;
 
         for (auto &nodes : level_nodes) {
             for (auto node : nodes) {
@@ -94,13 +92,20 @@ namespace abo::util {
 
                 then_node = Cudd_NotCond(then_node, Cudd_IsComplement(node));
                 else_node = Cudd_NotCond(else_node, Cudd_IsComplement(node));
-                indent(); outfile <<"\""<<node<<"\" -> \""<<then_node<<"\" [arrowhead = none];"<<std::endl;
-                indent(); outfile <<"\""<<node<<"\" -> \""<<else_node<<"\" [arrowhead = none, style = dashed];"<<std::endl;
+                indent(); output <<"\""<<node<<"\" -> \""<<then_node<<"\" [arrowhead = none];"<<std::endl;
+                indent(); output <<"\""<<node<<"\" -> \""<<else_node<<"\" [arrowhead = none, style = dashed];"<<std::endl;
             }
         }
 
         indentation--;
-        outfile <<"}"<<std::endl;
+        output <<"}"<<std::endl;
+    }
+
+    void dump_dot_readable_to_file(const Cudd &mgr, const BDD &b, std::string filename) {
+        std::ofstream outfile;
+        outfile.open(filename, std::ios::out | std::ios::trunc);
+
+        dump_dot_readable(mgr, b, outfile);
     }
 
 }
