1df480c466ec2f7f49fa7351164d4d80030dcf02
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-11-28 17:15:15 +0100

    Actually perform tests.

--- tests/dump_dot_test.cpp
+++ tests/dump_dot_test.cpp
@@ -5,17 +5,25 @@
 
 #include <dump_dot.hpp>
 
+#include <sstream>
 #include <iostream>
 
 TEST_CASE("Display BDD in a fancy way") {
-  std::cout << "FOOO\n";
+
 
   Cudd mgr(2);
   std::vector<std::string> names{"and"};
 
   BDD and_bdd = abo::example_bdds::and_bdd(mgr, 2);
 
-  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},std::cout, names);
-  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},std::cout);
-  abo::util::dump_dot_readable(mgr, and_bdd,std::cout);
+
+  std::stringstream s1, s2, s3;
+
+
+  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},s1, names);
+  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},s2);
+  abo::util::dump_dot_readable(mgr, and_bdd,s3);
+
+
+  CHECK(s2.str() == s3.str());
 }
