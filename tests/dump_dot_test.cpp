bba30c13a115352df154f1d2931f072bdb2f6a33
Oliver Keszöcze <oliver.keszoecze@fau.de>
2019-11-29 09:09:02 +0100

    Add new test case.
    It turns out, that white spaces cause problems in the dot file. Issue #12
    has been opened for that.

--- tests/dump_dot_test.cpp
+++ tests/dump_dot_test.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Display BDD in a fancy way") {
 
 
   Cudd mgr(2);
-  std::vector<std::string> names{"and"};
+  std::vector<std::string> names{"and", "another and"};
 
   BDD and_bdd = abo::example_bdds::and_bdd(mgr, 2);
 
@@ -20,9 +20,14 @@ TEST_CASE("Display BDD in a fancy way") {
   std::stringstream s1, s2, s3;
 
 
-  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},s1, names);
+  abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd,and_bdd},s1, names);
   abo::util::dump_dot_readable(mgr, std::vector<BDD>{and_bdd},s2);
   abo::util::dump_dot_readable(mgr, and_bdd,s3);
+
+
+  std::cout << s1.str() << "\n";
+
+  std::cout << s3.str();
 
 
   CHECK(s2.str() == s3.str());
