3ea162f77c8524b7bcee7c137fe2f65a3264ac40
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-02-16 16:02:22 +0100

    Add breathe library to doc config

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.coverage',
-    'sphinx.ext.mathjax']
+    'sphinx.ext.mathjax',
+    'breathe']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -165,5 +166,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
+# Breathe Configuration
+breathe_default_project = "ABO"
