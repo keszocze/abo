99389b9860c0e6017bfbfaaf54712480c9fe1288
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-02-23 21:19:12 +0100

    Document the remaining approximation operators

--- src/approximation_operators/approximation_operators.hpp
+++ src/approximation_operators/approximation_operators.hpp
@@ -7,12 +7,48 @@
  */
 namespace abo::operators {
 
+    /**
+     * @brief subset_light_child Approximates the given BDD by setting one child of each node with a variable level
+     * between level_start and level_end to 0. The child to subset is chosen as the one with the smaller ON-set as computed by abo::util::count_minterms
+     * @param mgr The cudd object manager to create nodes in
+     * @param bdd The bdd to approximate
+     * @param level_start The variable level to start approximating from
+     * @param level_end The last variable level at which the approximation is applied (may be equal to level_start when approximating only one level)
+     * @return The approximated BDD
+     */
     BDD subset_light_child(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
+    /**
+     * @brief subset_light_child Approximates the given BDD by setting one child of each node with a variable level
+     * between level_start and level_end to 1. The child to superset is chosen as the one with the larger ON-set as computed by abo::util::count_minterms
+     * @param mgr The cudd object manager to create nodes in
+     * @param bdd The bdd to approximate
+     * @param level_start The variable level to start approximating from
+     * @param level_end The last variable level at which the approximation is applied (may be equal to level_start when approximating only one level)
+     * @return The approximated BDD
+     */
     BDD superset_heavy_child(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
+    /**
+     * @brief subset_light_child Approximates the given BDD by setting one child of each node with a variable level
+     * between level_start and level_end to 1. The child to superset is chosen as the one with the smaller ON-set as computed by abo::util::count_minterms
+     * @param mgr The cudd object manager to create nodes in
+     * @param bdd The bdd to approximate
+     * @param level_start The variable level to start approximating from
+     * @param level_end The last variable level at which the approximation is applied (may be equal to level_start when approximating only one level)
+     * @return The approximated BDD
+     */
     BDD superset_light_child(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
+    /**
+     * @brief subset_light_child Approximates the given BDD by setting one child of each node with a variable level
+     * between level_start and level_end to 0. The child to subset is chosen as the one with the larger ON-set as computed by abo::util::count_minterms
+     * @param mgr The cudd object manager to create nodes in
+     * @param bdd The bdd to approximate
+     * @param level_start The variable level to start approximating from
+     * @param level_end The last variable level at which the approximation is applied (may be equal to level_start when approximating only one level)
+     * @return The approximated BDD
+     */
     BDD subset_heavy_child(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
 
@@ -26,12 +62,13 @@ namespace abo::operators {
     BDD round(const Cudd &mgr, const BDD &bdd, unsigned int level);
 
     /**
-     * @brief New operation by
-     * @param mgr
-     * @param bdd
-     * @param level_start
-     * @param level_end
-     * @return
+     * @brief New operation introduced for testing purposes. Like for example subset_light_child, it chooses one child for each node to replace
+     * by a terminal node, but it chooses the child and the corresponding terminal to minimize the resulting error rate
+     * @param mgr The cudd object manager to create nodes in
+     * @param bdd The bdd to approximate
+     * @param level_start The variable level to start approximating from
+     * @param level_end The last variable level at which the approximation is applied (may be equal to level_start when approximating only one level)
+     * @return The approximated BDD
      */
     BDD round_best(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
@@ -44,7 +81,7 @@ namespace abo::operators {
      * @param level_end The lowest BDD level (always the level of the terminals in the original paper)
      * @return The rounded BDD
      */
-    BDD round_up(const Cudd &mgr, const BDD &bdd, unsigned int level);
+    BDD round_up(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
     /**
      * @brief Implements the round-down operation from Soeken2016
@@ -54,6 +91,6 @@ namespace abo::operators {
      * @param level_end The lowest BDD level (always the level of the terminals in the original paper)
      * @return The rounded BDD
      */
-    BDD round_down(const Cudd &mgr, const BDD &bdd, unsigned int level);
+    BDD round_down(const Cudd &mgr, const BDD &bdd, unsigned int level_start, unsigned int level_end);
 
 }
