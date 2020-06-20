7bbea7a2e3d66e52013c3da72baea4fa44deee7a
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-06-20 15:34:34 +0200

    function: add some basic operators

--- src/util/function.cpp
+++ src/util/function.cpp
@@ -1,4 +1,6 @@
 #include "function.hpp"
+#include <algorithm>
+#include <cassert>
 
 namespace abo::util {
 
@@ -6,6 +8,40 @@ Function::Function(const Cudd &mgr, const std::vector<BDD> &value, NumberReprese
     mgr(mgr),
     value(value),
     representation(representation)
-{ }
+{
+    assert(value.size() > 0);
+}
+
+BDD& Function::operator[](std::size_t index) {
+    return value.at(index);
+}
+
+const BDD& Function::operator[](std::size_t index) const {
+    return value.at(index);
+}
+
+Function Function::operator&(const BDD& b) const {
+    Function result = *this;
+    result &= b;
+    return result;
+}
+
+Function Function::operator|(const BDD& b) const {
+    Function result = *this;
+    result |= b;
+    return result;
+}
+
+void Function::operator&=(const BDD& b) {
+    for (BDD& own : value) {
+        own &= b;
+    }
+}
+
+void Function::operator|=(const BDD& b) {
+    for (BDD& own : value) {
+        own |= b;
+    }
+}
 
 }
--- src/util/function.hpp
+++ src/util/function.hpp
@@ -12,6 +12,16 @@ class Function
 public:
     Function(const Cudd &mgr, const std::vector<BDD> &value, NumberRepresentation representation);
 
+    // bit access
+    BDD& operator[](std::size_t index);
+    const BDD& operator[](std::size_t index) const;
+
+    // bit-wise operations
+    Function operator&(const BDD& b) const;
+    Function operator|(const BDD& b) const;
+    void operator&=(const BDD& b);
+    void operator|=(const BDD& b);
+
 private:
     Cudd mgr; // the manager is refcounted, each function can have a copy (safer than storing just a reference)
     std::vector<BDD> value;
